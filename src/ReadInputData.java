1c2a5b390cdf52e3b8ba61267ca9621cf0e7326c
cristian udrescu <udrescu_cristian@yahoo.com>
2014-10-31 12:35:10 +0100

    read input data

--- src/ReadInputData.java
+++ src/ReadInputData.java
@@ -2,4 +2,16 @@
 public class ReadInputData {
 
 	//citeste si returneaza o matrice de forma int[][]
+	public int[][] create_mat(int n){
+		
+		int[][] newMatrix = new int[n][n];
+		for(int i =0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				newMatrix[i][j] = i*j;
+			}
+		}
+		
+		return newMatrix;
+	}
+	
 }
