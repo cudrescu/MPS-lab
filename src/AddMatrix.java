1a73a84104f749c013b8fff76c397e26e5d4dfa2
Andrei <andrei.serban01@yahoo.com>
2014-10-31 13:30:11 +0200

    add matrix

--- src/AddMatrix.java
+++ src/AddMatrix.java
@@ -3,4 +3,16 @@ public class AddMatrix {
 	
 	//o metoda add care primeste 2 matrici de forma int[][] 
 	//si returneaza matricea rezultat
+	
+	public int[][] add(int m1[][], int m2[][]) {
+		int res[][] = new int[m1.length][m2.length];
+		
+		for (int i = 0; i < res.length; i++) {
+			for (int j = 0; j < res[i].length; j++) {
+				res[i][j] = m1[i][j] + m2[i][j];
+			}
+		}
+		
+		return res;
+	}
 }
